77d0831fccec246874023af5af1be7f10888d9b9
EugeneLevkovych <eugenelevkovych@yahoo.com>
2025-04-27 13:40:47 +0300

    delete init items

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,10 +1,4 @@
-import Item from "./item";
-
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
+import Item from "./Item";
 
 export default function PackingList({ items }) {
   return (
